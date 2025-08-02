1c06cab417a9bdeac81fa309f4d32672233c412a
TT <tiffanytongxy@gmail.com>
2025-08-02 09:50:52 +0800

    Initial Commit

---
+++ Stickman/StickmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickmanApp.swift
+//  Stickman
+//
+//  Created by Tiffany on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct StickmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
